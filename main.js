6e145283c1b8d4c355d703a523550b3942c2ea52
nihadahmadov <sd80l0rbh@code.edu.az>
2023-10-20 01:17:02 +0400

    first commit

---
+++ main.js
@@ -0,0 +1,90 @@
+
+// let a = "10";
+// let b = "10";
+
+// if (a > b) {
+//   console.log("a b den boyukdur");
+// }
+// if ((a = b)) {
+//   console.log("a b e beraberdir");
+// } else if (a < b) {
+//   console.log("a b den kicikdir");
+// }
+
+
+
+
+// let students = [
+//   { id: 1, name: "Ali", surname: "Alisoy", age: 20, grade: 95 },
+
+//   { id: 2, name: "Vusala", surname: "Nabiyeva", age: 34, grade: 77 },
+
+//   { id: 3, name: "Aliya", surname: "Gurbanzade", age: 14, grade: 98 },
+
+//   { id: 4, name: "Sabir", surname: "Nuruyev", age: 15, grade: 54 },
+
+//   { id: 5, name: "Mehman", surname: "Heydarov", age: 22, grade: 100 },
+
+//   { id: 6, name: "Sevana", surname: "Mammadli", age: 41, grade: 35 },
+
+//   { id: 7, name: "Ilham", surname: "Babayev", age: 24, grade: 95 },
+
+//   { id: 8, name: "Namiq", surname: "Gulahmedov", age: 20, grade: 95 },
+
+//   { id: 9, name: "Aygun", surname: "Kazimova", age: 24, grade: 100 },
+// ];
+
+
+
+
+// let yeniArray = [];
+// for (let i = 0; i < students.length; i++) {
+//   if (i % 2 === 0) {
+//     yeniArray.push(students[i]);
+//   }
+// }
+// console.log(yeniArray);
+
+
+
+
+// for (const element of students) {
+//   if(element.id % 2 == 1){
+//     console.log(element.id);
+//   }
+// }
+
+
+
+
+
+// let newArray = []
+// for (const element of students) {
+//   if( element.age > 20){
+//     newArray.push(element);
+//   }
+// }
+// console.log(newArray)
+
+
+
+
+
+// let yeniArray = [];
+// for (const element of students) {
+//   if(element.id % 2 ===0  && element.grade > 90){
+//     yeniArray.push(element);
+//   }
+// }
+// console.log(yeniArray);
+
+
+
+
+// let yeniArray = [];
+// for (let i = 0; i < students.length; i++) {
+//   if (students[i].grade === 100 || students[i].grade === 95) {
+//     yeniArray.push(students[i].name);
+//   }
+// }
+// console.log(yeniArray);
